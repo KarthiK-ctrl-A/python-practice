5ca3a050a4d3377d8770f20e910e6664a8bea6a2
Karthik-ctrl-A <saikarthik4me@gmail.com>
2021-06-06 08:20:43 +0530

    commit-6/6

---
+++ breakingrecords.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    cntmax = 0
+    cntmin = 0
+    l = min(scores)
+    m = max(scores)
+
+    for i in scores:
+        if scores[i]>l
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
